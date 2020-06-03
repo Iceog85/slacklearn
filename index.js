b99323c87ed192146314632cb16308e62f7e9736
Tomek <okrajek.tomasz@gmail.com>
2020-06-03 09:55:14 +0200

    usuniecie jedengo web serwera

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.use('/my/path', slackEvents.requestListener());
 // Example: If you're using a body parser, always put it after the event adapter in the middleware stack
 app.use(bodyParser());
 
-// Initialize a server for the express app - you can skip this and the rest if you prefer to use app.listen()
-const server = createServer(app);
-server.listen(port, () => {
-  // Log a message when the server is ready
-  console.log(`Listening for events on ${server.address().port}`);
-});
+
 
 slackEvents.on('message', (event) => {
   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
