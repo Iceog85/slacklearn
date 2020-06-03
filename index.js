593befd3174b6cda83bd6d6a7f3d694e3cc6926e
Tomek <okrajek.tomasz@gmail.com>
2020-06-03 10:19:37 +0200

    interakcja.js

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const { createServer } = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createEventAdapter } = require('@slack/events-api');
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const port = process.env.PORT || 28410;
-const slackEvents = createEventAdapter(slackSigningSecret);
 
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
 // Create an express application
 const app = express();
 
 // Plug the adapter in as a middleware
-app.use('/my/path', slackEvents.requestListener());
+app.use('/slack/events', slackEvents.requestListener());
 
 // Example: If you're using a body parser, always put it after the event adapter in the middleware stack
 app.use(bodyParser());
-
-
 
 slackEvents.on('message', (event) => {
   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
 
+slackEvents.on('app_mention', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  slackEvents.message
+
+});
+
+// Tworzenie serwera
 (async () => {
   const server = await slackEvents.start(port);
   console.log(`Listening for events on ${server.address().port}`);
